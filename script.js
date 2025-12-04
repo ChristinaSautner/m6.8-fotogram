31014537e8298720b63e10122580c74d659fb676
Christina Sautner <christina.sautner@gmx.de>
2025-12-04 12:06:56 +0100

    createt functions "openDialog" & "closeDialog" (onclicks to be set)

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 let ImageTitles = [
-'beach-mountain-snow-sand.jpg',
-'cloud-sea-village.jpg',
-'field-tree-pink-sunset.jpg',
-'forest-tree-tube.jpg',
-'lake-dessert-mountain.jpg',
-'road-idyllic-village.jpg',
-'scotland-cliff.jpg'
+    'beach-mountain-snow-sand.jpg',
+    'cloud-sea-village.jpg',
+    'field-tree-pink-sunset.jpg',
+    'forest-tree-tube.jpg',
+    'lake-dessert-mountain.jpg',
+    'road-idyllic-village.jpg',
+    'scotland-cliff.jpg'
 ];
 
 let Images = [];
+
+// function openDialog() / closeDialog()
+let dialogRef = document.getElementById('dialogImage'); 
+
 
 function renderImages() {
     let contentRef = document.getElementById('thumbnails');
@@ -16,4 +20,14 @@ function renderImages() {
      * pull images
      * show images as thumbnails
      */
+}
+
+// onklick-event auf fotos setzen
+function openDialog() {
+    dialogRef.showModal();
+}
+
+// onklick-event im dialog setzen
+function closeDialog() {
+    dialogRef.close();
 }
