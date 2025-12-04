73162ee127a37210f779cfc47015d5a599540e49
Christina Sautner <christina.sautner@gmx.de>
2025-12-04 13:11:27 +0100

    created Array with images

--- script.js
+++ script.js
@@ -1,17 +1,59 @@
-let ImageTitles = [
-    'beach-mountain-snow-sand.jpg',
-    'cloud-sea-village.jpg',
-    'field-tree-pink-sunset.jpg',
-    'forest-tree-tube.jpg',
-    'lake-dessert-mountain.jpg',
-    'road-idyllic-village.jpg',
-    'scotland-cliff.jpg'
+let Images = [
+    '<img src="./beach-mountain-snow-sand.jpg" alt="Beach in front of a Mountain">',
+    '<img src="./chow-chow-dogs-car.jpg" alt="dogs enjoying the cool breeze">',
+    '<img src="./cloud-sea-village.jpg" alt="Ocean of Clouds">',
+    '<img src="./crow-crazy.jpg" alt="fuzzy Crow">',
+    '<img src="./crow.jpg" alt="Crow">',
+    '<img src="./deers-field.jpg" alt="wild Deers">',
+    '<img src="./ducklings-ducks.jpg" alt="cuddled up Ducklings">',
+    '<img src="./elephants.jpg" alt="playful Elephants">',
+    '<img src="./field-tree-pink-sunset.jpg" alt="single Tree at beautiful Sunset">',
+    '<img src="./forest-tree-tube.jpg" alt="mystic Trees">',
+    '<img src="./fox-snow.jpg" alt="Fox in Snow">',
+    '<img src="./giraffs-tree.jpg" alt="Giraffs with high Tree">',
+    '<img src="./lake-dessert-mountain.jpg" alt="Lake at mounty Desert">',
+    '<img src="./lamb-sheep.jpg" alt="mother Sheep with Lamb">',
+    '<img src="./leopards.jpg" alt="fluffy Leopards">',
+    '<img src="./lionesses-lions-night.jpg" alt="Lionesses at night">',
+    '<img src="./monkeys.jpg" alt="little Monkeys">',
+    '<img src="./rhinos.jpg" alt="mother Rhino with Baby">',
+    '<img src="./road-idyllic-village.jpg" alt="idyllic Road to Village">',
+    '<img src="./scotland-cliff.jpg" alt="scottish Cliff">',
+    '<img src="./sparrows.jpg" alt="Sparrows on Branch">',
+    '<img src="./vultures-geier.jpg" alt="Vultures in Greens">',
+    '<img src="./zebras.jpg" alt="Zebras in Greens">'
 ];
 
-let Images = [];
+
+let ImageTitles = [
+    'beach-mountain-snow-sand.jpg',
+    'chow-chow-dogs-car.jpg',
+    'cloud-sea-village.jpg',
+    'crow-crazy.jpg',
+    'crow.jpg',
+    'deers-field.jpg',
+    'ducklings-ducks.jpg',
+    'elephants.jpg',
+    'field-tree-pink-sunset.jpg',
+    'forest-tree-tube.jpg',
+    'fox-snow.jpg',
+    'giraffs-tree.jpg',
+    'lake-dessert-mountain.jpg',
+    'lamb-sheep.jpg',
+    'leopards.jpg',
+    'lionesses-lions-night.jpg',
+    'monkeys.jpg',
+    'rhinos.jpg',
+    'road-idyllic-village.jpg',
+    'scotland-cliff.jpg',
+    'sparrows.jpg',
+    'vultures-geier.jpg',
+    'zebras.jpg'
+];
+
 
 // function openDialog() / closeDialog()
-let dialogRef = document.getElementById('dialogImage'); 
+let dialogRef = document.getElementById('dialogImage');
 
 
 function renderImages() {
