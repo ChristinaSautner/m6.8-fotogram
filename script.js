afa85a351ef983c62b0eaab27a13872643655242
Christina Sautner <christina.sautner@gmx.de>
2025-12-04 13:36:54 +0100

    make thumbnail-view work  \(^o^)/

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 let Images = [
-    '<img src="./beach-mountain-snow-sand.jpg" alt="Beach in front of a Mountain">',
-    '<img src="./chow-chow-dogs-car.jpg" alt="dogs enjoying the cool breeze">',
-    '<img src="./cloud-sea-village.jpg" alt="Ocean of Clouds">',
-    '<img src="./crow-crazy.jpg" alt="fuzzy Crow">',
-    '<img src="./crow.jpg" alt="Crow">',
-    '<img src="./deers-field.jpg" alt="wild Deers">',
-    '<img src="./ducklings-ducks.jpg" alt="cuddled up Ducklings">',
-    '<img src="./elephants.jpg" alt="playful Elephants">',
-    '<img src="./field-tree-pink-sunset.jpg" alt="single Tree at beautiful Sunset">',
-    '<img src="./forest-tree-tube.jpg" alt="mystic Trees">',
-    '<img src="./fox-snow.jpg" alt="Fox in Snow">',
-    '<img src="./giraffs-tree.jpg" alt="Giraffs with high Tree">',
-    '<img src="./lake-dessert-mountain.jpg" alt="Lake at mounty Desert">',
-    '<img src="./lamb-sheep.jpg" alt="mother Sheep with Lamb">',
-    '<img src="./leopards.jpg" alt="fluffy Leopards">',
-    '<img src="./lionesses-lions-night.jpg" alt="Lionesses at night">',
-    '<img src="./monkeys.jpg" alt="little Monkeys">',
-    '<img src="./rhinos.jpg" alt="mother Rhino with Baby">',
-    '<img src="./road-idyllic-village.jpg" alt="idyllic Road to Village">',
-    '<img src="./scotland-cliff.jpg" alt="scottish Cliff">',
-    '<img src="./sparrows.jpg" alt="Sparrows on Branch">',
-    '<img src="./vultures-geier.jpg" alt="Vultures in Greens">',
-    '<img src="./zebras.jpg" alt="Zebras in Greens">'
+    '<img src="./assets/images/beach-mountain-snow-sand.jpg" alt="Beach in front of a Mountain">',
+    '<img src="./assets/images/chow-chow-dogs-car.jpg" alt="dogs enjoying the cool breeze">',
+    '<img src="./assets/images/cloud-sea-village.jpg" alt="Ocean of Clouds">',
+    '<img src="./assets/images/crow-crazy.jpg" alt="fuzzy Crow">',
+    '<img src="./assets/images/crow.jpg" alt="Crow">',
+    '<img src="./assets/images/deers-field.jpg" alt="wild Deers">',
+    '<img src="./assets/images/ducklings-ducks.jpg" alt="cuddled up Ducklings">',
+    '<img src="./assets/images/elephants.jpg" alt="playful Elephants">',
+    '<img src="./assets/images/field-tree-pink-sunset.jpg" alt="single Tree at beautiful Sunset">',
+    '<img src="./assets/images/forest-tree-tube.jpg" alt="mystic Trees">',
+    '<img src="./assets/images/fox-snow.jpg" alt="Fox in Snow">',
+    '<img src="./assets/images/giraffs-tree.jpg" alt="Giraffs with high Tree">',
+    '<img src="./assets/images/lake-dessert-mountain.jpg" alt="Lake at mounty Desert">',
+    '<img src="./assets/images/lamb-sheep.jpg" alt="mother Sheep with Lamb">',
+    '<img src="./assets/images/leopards.jpg" alt="fluffy Leopards">',
+    '<img src="./assets/images/lionesses-lions-night.jpg" alt="Lionesses at night">',
+    '<img src="./assets/images/monkeys.jpg" alt="little Monkeys">',
+    '<img src="./assets/images/rhinos.jpg" alt="mother Rhino with Baby">',
+    '<img src="./assets/images/road-idyllic-village.jpg" alt="idyllic Road to Village">',
+    '<img src="./assets/images/scotland-cliff.jpg" alt="scottish Cliff">',
+    '<img src="./assets/images/sparrows.jpg" alt="Sparrows on Branch">',
+    '<img src="./assets/images/vultures-geier.jpg" alt="Vultures in Greens">',
+    '<img src="./assets/images/zebras.jpg" alt="Zebras in Greens">'
 ];
 
 
@@ -58,6 +58,7 @@ let dialogRef = document.getElementById('dialogImage');
 
 function renderImages() {
     let thumbnails = document.getElementById('thumbnails');
+    thumbnails.innerHTML = Images;
     /**
      * pull images
      * show images as thumbnails
