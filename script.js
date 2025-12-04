1256a9dab69bbed03378e85c4cfc918b9f92ad64
Christina Sautner <christina.sautner@gmx.de>
2025-12-04 11:22:37 +0100

    added id to html-div (thumbnails). added array "images" to js.

---
+++ script.js
@@ -0,0 +1,9 @@
+let images = [
+'beach-mountain-snow-sand.jpg',
+'cloud-sea-village.jpg',
+'field-tree-pink-sunset.jpg',
+'forest-tree-tube.jpg',
+'lake-dessert-mountain.jpg',
+'road-idyllic-village.jpg',
+'scotland-cliff.jpg'
+];
